b83c3083fba86327c931ff604905826e9d0d8a16
Chris Minn <105378107+minnce@users.noreply.github.com>
2023-05-14 14:39:26 -0500

    Create chefPlaysLudo.cpp

---
+++ chefPlaysLudo.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t, x;
+	cin >> t;
+	for (int i = 0; i < t; i++) {
+	    cin >> x;
+	    if (x == 6) {cout << "YES\n";}
+	    else {cout << "NO\n";}
+	}
+	return 0;
+}
