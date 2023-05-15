d7c3910552ddd37ba169aa0879b169b14d997a3f
Chris Minn <105378107+minnce@users.noreply.github.com>
2023-05-15 02:24:45 -0500

    Create candyDivision.cpp

---
+++ candyDivision.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t, x;
+	cin >> t;
+	for (int i = 0; i < t; i++) {
+	    cin >> x;
+	    if (x%3 != 0) {
+	        cout << "NO\n";
+	    }
+	    else {
+	        cout << "YES\n";
+	    }
+	}
+	return 0;
+}
