1a3cc6c3a6118e2c6a88de959e2d1f83dab19287
Chris Minn <105378107+minnce@users.noreply.github.com>
2023-05-15 22:16:17 -0500

    Create oneEpisode.cpp

---
+++ oneEpisode.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t,x;
+    cin >> t;
+    for (int i = 0; i < t; i++) {
+        cin >> x;
+        if (x <= 24) {
+            cout << "NO\n";
+        }
+        else {
+            cout << "YES\n";
+        }
+    }
+	return 0;
+}
