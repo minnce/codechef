9a2cc3613b0ce0beb265861010f82ed7acd7984a
Chris Minn <105378107+minnce@users.noreply.github.com>
2023-05-15 22:06:57 -0500

    Create sumIt.cpp

---
+++ sumIt.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t,a,b,c;
+    cin >> t;
+    for (int i = 0; i < t; i++) {
+        cin >> a >> b >> c;
+        if (a+b == c) {
+            cout << "YES\n";
+        }
+        else {
+            cout << "NO\n";
+        }
+    }
+	return 0;
+}
