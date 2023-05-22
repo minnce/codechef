c51ecce2117b59779d71da8deeb9d926aecc913e
Chris Minn <105378107+minnce@users.noreply.github.com>
2023-05-22 16:57:08 -0500

    Create appleOrange.cpp

---
+++ appleOrange.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int x,a,b;
+	cin >> x >> a>> b;
+	if (x < a+b) {
+	    cout <<"NO\n";
+	}
+	else {
+	    cout << "YES\n";
+	}
+	return 0;
+}
