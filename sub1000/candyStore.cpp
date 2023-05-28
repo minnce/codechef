a6d18a2e1aa4990597d1d14823cca4ccdc2c6e2e
Chris Minn <105378107+minnce@users.noreply.github.com>
2023-05-28 16:38:26 -0500

    Create candyStore.cpp

---
+++ sub1000/candyStore.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#define ll long long
+#define pb push_back
+#define F first
+#define S second
+#define vt vector
+using namespace std;
+
+void solve()
+{
+    int x,y;
+    cin >> x >> y;
+    if (x>=y) {
+        cout << y << endl;
+    }
+    else {
+        cout << (abs((y-x)*2)+x) << endl;
+    }
+}
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0), cout.tie(0);
+   	ll t;
+    	cin >> t;
+    	while (t--)
+    	{
+        	solve();
+    	}
+    	return 0;
+}
