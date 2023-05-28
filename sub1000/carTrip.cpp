6e1de3e03ba3eaa75f3d468c2f133f722a190533
Chris Minn <105378107+minnce@users.noreply.github.com>
2023-05-28 16:07:56 -0500

    Create carTrip.cpp

---
+++ sub1000/carTrip.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#define ll long long
+#define pb push_back
+#define F first
+#define S second
+#define vt vector
+using namespace std;
+
+void solve()
+{
+    int x;
+    cin >> x;
+    if (x<=300) {
+        cout << 3000 << endl;
+    }
+    else {
+        cout << x*10 << endl;
+    }
+}
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0), cout.tie(0);
+   	ll t;
+    	cin >> t;
+    	while (t--)
+    	{
+        	solve();
+    	}
+    	return 0;
+}
