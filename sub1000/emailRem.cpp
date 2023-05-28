ba330fe1c14d6283a16d80fd678655eaa42f6d3f
Chris Minn <105378107+minnce@users.noreply.github.com>
2023-05-28 16:10:04 -0500

    Update emailRem.cpp

--- sub1000/emailRem.cpp
+++ sub1000/emailRem.cpp
@@ -7,22 +7,11 @@
 #define vt vector
 using namespace std;
 
-void solve()
-{
-    int n,u;
-    cin >> n>> u;
-    cout << n-u << endl;
-}
+
 
 int main()
 {
-	ios::sync_with_stdio(0);
-	cin.tie(0), cout.tie(0);
-   	ll t;
-    	cin >> t;
-    	while (t--)
-    	{
-        	solve();
-    	}
-    	return 0;
+	int n,u;
+    cin >> n>> u;
+    cout << n-u << endl;
 }
