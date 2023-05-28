6fac528287139e264722cc72416d77d749a6bbc2
Chris Minn <105378107+minnce@users.noreply.github.com>
2023-05-28 16:39:23 -0500

    Create scaleneTri.cpp

---
+++ sub1000/scaleneTri.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#define ll long long
+#define pb push_back
+#define F first
+#define S second
+#define vt vector
+using namespace std;
+
+void solve()
+{
+    int a,b,c;
+    cin >> a >> b >> c;
+    if (a!=b&&b!=c&&a!=c) {
+        cout << "YES\n";
+    }
+    else {
+        cout << "NO\n";
+    }
+}
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0), cout.tie(0);
+   	ll t;
+    	cin >> t;
+    	while (t--)
+    	{
+        	solve();
+    	}
+    	return 0;
+}
