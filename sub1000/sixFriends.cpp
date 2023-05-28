ba48ae24f5c71d2f4e7068c508edbf157fbfc6a4
Chris Minn <105378107+minnce@users.noreply.github.com>
2023-05-28 16:11:34 -0500

    Create sixFriends.cpp

---
+++ sub1000/sixFriends.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#define ll long long
+#define pb push_back
+#define F first
+#define S second
+#define vt vector
+using namespace std;
+
+void solve()
+{
+    int x,y;
+    cin >> x >> y;
+    cout << (min(x*3,y*2)) << endl;
+}
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0), cout.tie(0);
+   	ll t;
+    	cin >> t;
+    	while (t--)
+    	{
+        	solve();
+    	}
+    	return 0;
+}
