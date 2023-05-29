065f64c1b32430c7d51b8e63800748e9ca51bae7
Chris Minn <105378107+minnce@users.noreply.github.com>
2023-05-29 01:22:10 -0500

    Create chefDigits.cpp

---
+++ 1200-1400/chefDigits.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#define ll long long
+#define pb push_back
+#define F first
+#define S second
+#define vt vector
+using namespace std;
+
+void solve()
+{
+    string s;
+    cin >> s;
+    int dummy1 = count(s.begin(),s.end(),'1');
+    int dummy2 = count(s.begin(),s.end(),'0');
+    if (dummy2==1||dummy1==1) {
+        cout << "Yes\n";
+    }
+    else {
+        cout << "No\n";
+    }
+}
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0), cout.tie(0);
+   	ll t;
+    	cin >> t;
+    	while (t--)
+    	{
+        	solve();
+    	}
+    	return 0;
+}
