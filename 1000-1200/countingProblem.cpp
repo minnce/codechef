92a6ade248398d4cd7c1cadb46a7179d3f988db9
Chris Minn <105378107+minnce@users.noreply.github.com>
2023-05-29 02:26:43 -0500

    Create countingProblem.cpp

---
+++ 1000-1200/countingProblem.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#define ll long long
+#define pb push_back
+#define F first
+#define S second
+#define vt vector
+using namespace std;
+
+void solve()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    int o = 0;
+    for (int i = 0; i < n; i++) {
+        if (arr[i]%2==1) {
+            o++;
+        }
+    }
+    if (o%2==0) {
+        cout << "YES\n";
+    }
+    else {
+        cout << "NO\n";
+    }
+}
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0), cout.tie(0);
+   	ll t;
+    	cin >> t;
+    	while (t--)
+    	{
+        	solve();
+    	}
+    	return 0;
+}
