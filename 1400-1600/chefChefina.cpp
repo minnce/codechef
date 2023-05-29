90f01e656a713095052019488167a7b873cae4b0
Chris Minn <105378107+minnce@users.noreply.github.com>
2023-05-28 20:59:27 -0500

    Create chefChefina.cpp

---
+++ 1400-1600/chefChefina.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#define ll long long
+#define pb push_back
+#define F first
+#define S second
+#define vt vector
+using namespace std;
+
+void solve()
+{
+    int x,y;
+    cin >> x >> y;
+    int dummy = x&y;
+    cout << ((x^dummy)+(y^dummy)) << endl;
+}
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0), cout.tie(0);
+   	ll t;
+    	cin >> t;
+    	while (t--)
+    	{
+        	solve();
+    	}
+    	return 0;
+}
