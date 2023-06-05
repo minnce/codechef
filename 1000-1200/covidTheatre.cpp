1ab8e39bf067cd5fb93bbbb7ae0f7520a4368c61
Chris Minn <105378107+minnce@users.noreply.github.com>
2023-06-05 00:10:23 -0500

    Create covidTheatre.cpp

---
+++ 1000-1200/covidTheatre.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#define ll long long
+#define pb push_back
+#define F first
+#define S second
+#define vt vector
+using namespace std;
+
+void solve()
+{
+    int n,m;
+    cin >> n >> m;
+    if (n%2==1) {
+        n++;
+    }
+    if (m%2==1) {
+        m++;
+    }
+    cout << ((n/2)*(m/2)) << endl;
+}
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0), cout.tie(0);
+   	ll t;
+    	cin >> t;
+    	while (t--)
+    	{
+        	solve();
+    	}
+    	return 0;
+}
