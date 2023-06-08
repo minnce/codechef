ebf07c8a0bcfd599035b00cdd23b98d88ef0eb24
Chris Minn <105378107+minnce@users.noreply.github.com>
2023-06-08 11:07:14 -0500

    Create iq.cpp

---
+++ sub1000/iq.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#define ll long long
+#define pb push_back
+#define F first
+#define S second
+#define vt vector
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0), cout.tie(0);
+   	ll t;
+    	cin >> t;
+    	if (t+7>170) {
+    	    cout << "YES\n";
+    	}
+    	else {
+    	    cout << "NO\n";
+    	}
+    	return 0;
+}
