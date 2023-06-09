6d6d2a0a955c6852691f2475f40b9ab98b09f5ab
Chris Minn <105378107+minnce@users.noreply.github.com>
2023-06-09 03:52:22 -0500

    Create dominantElem.cpp

---
+++ 1000-1200/dominantElem.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#define ll long long
+#define pb push_back
+#define F first
+#define S second
+#define vt vector
+using namespace std;
+
+void solve()
+{
+    int n;
+    cin >> n;
+    map<int,int> m;
+    for (int i = 0; i < n; i++) {
+        int dummy;
+        cin >> dummy;
+        m[dummy]++;
+    }
+    vector<int> v;
+    for (const auto& elem : m) {
+        v.pb(elem.second);
+    }
+    if (v[n-1]==v[n-2]&&v.size()!=1) {
+        cout << "NO\n";
+    }
+    else {
+        cout << "YES\n";
+    }
+}
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0), cout.tie(0);
+   	ll t;
+    	cin >> t;
+    	while (t--)
+    	{
+        	solve();
+    	}
+    	return 0;
+}
