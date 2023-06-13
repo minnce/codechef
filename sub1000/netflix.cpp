3d113dee185683fb6dd7cf9453aaaa0f7e5f97da
Chris Minn <105378107+minnce@users.noreply.github.com>
2023-06-13 00:31:10 -0500

    Create netflix.cpp

---
+++ sub1000/netflix.cpp
@@ -0,0 +1,51 @@
+#pragma GCC optimize("O2,no-stack-protector,unroll-loops,fast-math")
+#include <iostream>
+#include <bits/stdc++.h>
+#define ll long long
+#define pb push_back
+#define F first
+#define S second
+#define uset unordered_set
+#define umap unordered_map
+#define MP make_pair;
+#define vt vector
+#define all(x) begin(x), end(x)
+#define sz(x) (int)x.size()
+#define REP(i, a) for (int i = 0; i < a; i++)
+#define pii pair<int, int>;
+#define pll pair<ll, ll>;
+
+using namespace std;
+
+typedef vector<ll> vl;
+typedef pair<ll,ll> pl;
+
+void solve()
+{
+    int a,b,c,x;
+    cin >> a >> b >> c >> x;
+    vt<int> v;
+    v.pb(a);
+    v.pb(b);
+    v.pb(c);
+    sort(v.begin(),v.end());
+    if (v[2]+v[1]>=x) {
+        cout << "YES\n";
+    }
+    else {
+        cout << "NO\n";
+    }
+}
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0), cout.tie(0);
+   	ll t;
+    	cin >> t;
+    	while (t--)
+    	{
+        	solve();
+    	}
+    	return 0;
+}
