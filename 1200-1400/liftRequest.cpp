f3533e6523adaed15645cd649331b070e90325d7
Chris Minn <105378107+minnce@users.noreply.github.com>
2023-06-24 03:01:31 -0500

    Create liftRequest.cpp

---
+++ 1200-1400/liftRequest.cpp
@@ -0,0 +1,50 @@
+#pragma GCC optimize("O2,no-stack-protector,unroll-loops,fast-math")
+#include <iostream>
+#include <bits/stdc++.h>
+#define ll long long
+#define PB push_back
+#define F first
+#define S second
+#define uset unordered_set
+#define umap unordered_map
+#define MP make_pair;
+#define vt vector
+#define all(x) begin(x), end(x)
+#define sz(x) (int)x.size()
+#define REP(i, a) for (int i = 0; i < a; i++)
+#define pii pair<int, int>;
+#define pll pair<ll, ll>;
+
+using namespace std;
+
+typedef vector<ll> vl;
+typedef pair<ll,ll> pl;
+
+void solve()
+{
+    int n,q;
+    cin >> n >> q;
+    int curr = 0;
+    ll tot = 0;
+    for (int i = 0; i < q; i++) {
+        int d1,d2;
+        cin >> d1 >> d2;
+        tot+=abs(curr-d1);
+        tot+=abs(d1-d2);
+        curr = d2;
+    }
+    cout << tot << endl;
+}
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0), cout.tie(0);
+   	ll t;
+    	cin >> t;
+    	while (t--)
+    	{
+        	solve();
+    	}
+    	return 0;
+}
