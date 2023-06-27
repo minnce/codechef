4bf30aa1b924f88404a9a7ff4e88c0d43026ade1
Chris Minn <105378107+minnce@users.noreply.github.com>
2023-06-27 09:15:22 -0500

    Create controlPollution.cpp

---
+++ 1400-1600/controlPollution.cpp
@@ -0,0 +1,44 @@
+#pragma GCC optimize("O2,no-stack-protector,unroll-loops,fast-math")
+#include <iostream>
+#include <bits/stdc++.h>
+#define ll long long
+#define PB push_back
+#define F first
+#define S second
+#define uset unordered_set
+#define umap unordered_map
+#define MP make_pair;
+#define vt vector
+#define all(x) begin(x), end(x)
+#define sz(x) (int)x.size()
+#define REP(i, a) for (int i = 0; i < a; i++)
+#define pii pair<int, int>;
+#define pll pair<ll, ll>;
+
+using namespace std;
+
+typedef vector<ll> vl;
+typedef pair<ll,ll> pl;
+
+void solve()
+{
+    int n,x,y;
+    cin >> n >> x >> y;
+    int d = ((n/100)*x)+(((n%100+4-1)/4)*y);
+    int e = (((n+4-1)/4)*y);
+    int f = ((n+99)/100*x);
+    cout << min(d,min(e,f)) << endl;
+}
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0), cout.tie(0);
+   	ll t;
+    	cin >> t;
+    	while (t--)
+    	{
+        	solve();
+    	}
+    	return 0;
+}
