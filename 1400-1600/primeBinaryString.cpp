215c957e5b7925d9759fab44fec4c0fbe1bc42d9
Chris Minn <105378107+minnce@users.noreply.github.com>
2023-06-28 12:03:14 -0500

    Create primeBinaryString.cpp

---
+++ 1400-1600/primeBinaryString.cpp
@@ -0,0 +1,53 @@
+#pragma GCC optimize("O2,no-stack-protector,unroll-loops,fast-math")
+#include <iostream>
+#include <bits/stdc++.h>
+#define ll long long
+#define PB push_back
+#define F first
+#define S second
+#define uset unordered_set
+#define umap unordered_map
+#define MP make_pair;
+#define vt vector
+#define all(x) begin(x), end(x)
+#define sz(x) (int)x.size()
+#define REP(i, a) for (int i = 0; i < a; i++)
+#define pii pair<int, int>;
+#define pll pair<ll, ll>;
+
+using namespace std;
+
+typedef vector<ll> vl;
+typedef pair<ll,ll> pl;
+
+void solve()
+{
+    string s;
+    cin >> s;
+    bool flag = false;
+    for (int i = 1; i < s.size(); i++) {
+        if ((s[i]=='1'&&s[i-1]=='1')||(s[i]=='0'&&s[i-1]=='1')) {
+            flag = true;
+            break;
+        }
+    }
+    if (flag==true) {
+        cout << "YES\n";
+    }
+    else {
+        cout << "NO\n";
+    }
+}
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0), cout.tie(0);
+   	ll t;
+    	cin >> t;
+    	while (t--)
+    	{
+        	solve();
+    	}
+    	return 0;
+}
