5c7a59cf00e535b5812d27ee709ca616755ec48d
Chris Minn <105378107+minnce@users.noreply.github.com>
2023-07-01 18:12:26 -0500

    Create morningCommute.cpp

---
+++ 1200-1400/morningCommute.cpp
@@ -0,0 +1,62 @@
+#pragma GCC optimize("O2,no-stack-protector,unroll-loops,fast-math")
+#include <iostream>
+#include <bits/stdc++.h>
+#define ll long long
+#define PB push_back
+#define F first
+#define S second
+#define uset unordered_set
+#define umap unordered_map
+#define MP make_pair
+#define vt vector
+#define all(x) begin(x), end(x)
+#define sz(x) (int)x.size()
+#define REP(i, a) for (int i = 0; i < a; i++)
+#define pii pair<int, int>;
+#define pll pair<ll, ll>;
+
+using namespace std;
+
+typedef vector<ll> vl;
+typedef pair<ll,ll> pl;
+
+void solve()
+{
+    int n;
+    cin >> n;
+    ll tot = 0;
+    for (int i = 0; i < n; i++) {
+        int d1,d2,d3;
+        cin >> d1>>d2>>d3;
+        if (tot<d1) {
+            tot=d1+d2;
+        }
+        else if (tot==d1) {
+            tot+=d2;
+        }
+        else {
+            if (tot>d1&&tot<(d1+d3)) {
+                tot = d1+d3+d2;
+            }
+            else if (tot>d1&&tot>=(d1+d3)) {
+                int d = tot-d1;
+                d=(d+d3-1)/d3;
+                tot=d1+(d*d3)+d2;
+            }
+        }
+    }
+    cout << tot << endl;
+}
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0), cout.tie(0);
+   	ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
