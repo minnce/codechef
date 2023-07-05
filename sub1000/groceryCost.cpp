7e70d376bfcbbb7741614cd80b99b3a91dee43f0
Chris Minn <105378107+minnce@users.noreply.github.com>
2023-07-05 18:23:24 -0500

    Create groceryCost.cpp

---
+++ sub1000/groceryCost.cpp
@@ -0,0 +1,54 @@
+#pragma GCC optimize("O2,no-stack-protector,unroll-loops,fast-math")
+#include <iostream>
+#include <bits/stdc++.h>
+#define ll long long
+#define PB push_back
+#define F first
+#define S second
+#define uset unordered_set
+#define umap unordered_map
+#define MP make_pair
+#define vt vector
+#define all(x) begin(x), end(x)
+#define sz(x) (int)x.size()
+#define REP(i, a) for (int i = 0; i < a; i++)
+#define pii pair<int, int>;
+#define pll pair<ll, ll>;
+
+using namespace std;
+
+typedef vector<ll> vl;
+typedef pair<ll,ll> pl;
+
+void solve()
+{
+    int a,b;
+    cin >>a>>b;
+    int arr[a], arr2[a];
+    ll tot = 0;
+    for (int i = 0; i< a;i++) {
+        cin >> arr[i];
+    }
+    for (int i = 0; i< a;i++) {
+        cin >> arr2[i];
+    }
+    for (int i = 0; i< a;i++) {
+        if (arr[i]>=b) {
+            tot+=arr2[i];
+        }
+    }
+    cout << tot << endl;
+}
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0), cout.tie(0);
+   	ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
