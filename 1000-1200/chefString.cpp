928ab80d4979ac4e681576cf93c9d8e0bfc63761
Chris Minn <105378107+minnce@users.noreply.github.com>
2023-07-07 21:07:14 -0500

    Create chefString.cpp

---
+++ 1000-1200/chefString.cpp
@@ -0,0 +1,48 @@
+#pragma GCC optimize("O2,no-stack-protector,unroll-loops,fast-math")
+#include <iostream>
+#include <bits/stdc++.h>
+#define ll long long
+#define PB push_back
+#define F first
+#define S second
+#define uset unordered_set
+#define umap unordered_map
+#define MP make_pair
+#define vt vector
+#define all(x) begin(x), end(x)
+#define sz(x) (int)x.size()
+#define REP(i, a) for (int i = 0; i < a; i++)
+#define pii pair<int, int>;
+#define pll pair<ll, ll>;
+
+using namespace std;
+
+typedef vector<ll> vl;
+typedef pair<ll,ll> pl;
+
+void solve()
+{
+    string s;
+    cin >> s;
+    int d1 =0, d2=0;
+    for (int i = 1; i < s.length(); i++) {
+        if ((s[i]=='x'&&s[i-1]=='y')||(s[i]=='y'&&s[i-1]=='x')) {
+            d1++;
+            i++;
+        }
+    }
+    cout << max(d1,d2) << endl;
+}
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0), cout.tie(0);
+   	ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
