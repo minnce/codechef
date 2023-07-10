32e559033465ff6987842b7f315d7e630382b9f0
Chris Minn <105378107+minnce@users.noreply.github.com>
2023-07-10 18:07:59 -0500

    Create nonAdjacentFlips.cpp

---
+++ 1000-1200/nonAdjacentFlips.cpp
@@ -0,0 +1,56 @@
+#pragma GCC optimize("O2,no-stack-protector,unroll-loops,fast-math")
+#include <iostream>
+#include <bits/stdc++.h>
+#define ll long long
+#define PB push_back
+#define F first
+#define S second
+#define uset unordered_set
+#define umap unordered_map
+#define MP make_pair
+#define vt vector
+#define all(x) begin(x), end(x)
+#define sz(x) (int)x.size()
+#define REP(i, a) for (int i = 0; i < a; i++)
+#define pii pair<int, int>;
+#define pll pair<ll, ll>;
+
+using namespace std;
+
+typedef vector<ll> vl;
+typedef pair<ll,ll> pl;
+
+void solve()
+{
+    int n; string s;
+    cin >> n >> s;
+    int flag=0; int flag2 = false;
+    for (int i = 0; i < n; i++) {
+        if (s[i]=='1') {
+            flag2=true;
+        }
+        if (s[i]=='1'&&s[i-1]=='1'&&i>=1) {
+            flag = 1;
+            break;
+        }
+    }
+    if (flag2==true)   {
+        cout << 1+flag<<endl;
+    }
+    else {
+        cout << flag <<endl;
+    }
+}
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0), cout.tie(0);
+   	ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
