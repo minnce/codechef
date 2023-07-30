d35cc14958eb02f46d560a6079bfa5d7301399fe
Chris Minn <105378107+minnce@users.noreply.github.com>
2023-07-30 15:31:13 -0500

    Create expensiveStep.cpp

---
+++ 1200-1400/expensiveStep.cpp
@@ -0,0 +1,55 @@
+#pragma GCC optimize("O2,no-stack-protector,unroll-loops,fast-math")
+#include <iostream>
+#include <bits/stdc++.h>
+#define ll long long
+#define db double
+#define MOD 1000000007
+#define PB push_back
+#define F first
+#define S second
+#define uset unordered_set
+#define umap unordered_map
+#define pq priority_queue
+#define MP make_pair
+#define vt vector
+#define ins insert
+#define __int128 int128
+#define all(x) begin(x), end(x)
+#define rall(x) rbegin(x), rend(x)
+#define sz(x) (int)x.size()
+#define REP(i, a) for (int i = 0; i < a; i++)
+#define pii pair<int, int>;
+#define setprf(x) setprecision(x) << fixed
+#define setpr(x) setprecision(x)
+#define pll pair<ll, ll>;
+
+using namespace std;
+
+typedef vector<ll> vl;
+typedef pair<ll,ll> pl;
+typedef long double ld;
+typedef complex<ld> cd;
+
+void solve()
+{
+     long long cv,we,er,yu,ui;
+        cin>>cv>>we>>er>>yu>>ui;
+        long long io = min(we,cv-we+cv/cv);
+        long long qq = min(er,cv-er+cv/cv);
+        long long rt = min(yu,cv-yu+cv/cv);
+        long long ty = min(ui,cv-ui+cv/cv);
+        cout<<min(abs(yu-we+yu*0)+abs(ui-er+er*0), min(rt,ty)+min(io,qq))<<endl;
+}
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0), cout.tie(0);
+   	ll t;
+    	cin >> t;
+    	while (t--)
+    	{
+        	solve();
+    	}
+    	return 0;
+}
