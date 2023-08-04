e2a2b58d6016c9cd4ae6c834b04d9b5d5423a1e9
Chris Minn <105378107+minnce@users.noreply.github.com>
2023-08-04 16:16:41 -0500

    Create xorPalindrome.cpp

---
+++ 1200-1400/xorPalindrome.cpp
@@ -0,0 +1,59 @@
+#pragma GCC optimize("O2,no-stack-protector,unroll-loops,fast-math")
+#include <iostream>
+#include <bits/stdc++.h>
+#define ll long long
+#define db double
+#define MOD 1000000007
+#define PB push_back
+#define F first
+#define S second
+#define uset unordered_set
+#define umap unordered_map
+#define pq priority_queue
+#define MP make_pair
+#define vt vector
+#define ins insert
+#define __int128 int128
+#define all(x) begin(x), end(x)
+#define rall(x) rbegin(x), rend(x)
+#define sz(x) (int)x.size()
+#define REP(i, a) for (int i = 0; i < a; i++)
+#define pii pair<int, int>;
+#define setprf(x) setprecision(x) << fixed
+#define setpr(x) setprecision(x)
+#define pll pair<ll, ll>;
+
+using namespace std;
+
+typedef vector<ll> vl;
+typedef pair<ll,ll> pl;
+typedef long double ld;
+typedef complex<ld> cd;
+
+void solve()
+{
+    int n; string s;
+    cin >> n >> s;
+    ll o =0,z=0;
+    REP (i,n) {
+        if (s[i]=='0') o++;
+        else z++;
+    }
+    if (abs(o-z)<=1||(o%2==0&&z%2==0)||(o%2==1&&z%2==0)||(z%2==1&&o%2==0)) {
+        cout << "YES\n";
+    }
+    else cout << "NO\n";
+}
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0), cout.tie(0);
+   	ll t;
+    	cin >> t;
+    	while (t--)
+    	{
+        	solve();
+    	}
+    	return 0;
+}
