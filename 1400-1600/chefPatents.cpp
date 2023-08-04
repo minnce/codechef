4ef34f85b45dfd8ca461299bb653fb306600cbf5
Chris Minn <105378107+minnce@users.noreply.github.com>
2023-08-04 02:59:27 -0500

    Create chefPatents.cpp

---
+++ 1400-1600/chefPatents.cpp
@@ -0,0 +1,64 @@
+#pragma GCC optimize("O2,no-stack-protector,unroll-loops,fast-math")
+#include <iostream>
+#include <bits/stdc++.h>
+#define ll long long
+#define db double
+#define MOD 1000000007
+#define PB push_back
+#define F first
+#define S second
+#define uset unordered_set
+#define umap unordered_map
+#define pq priority_queue
+#define MP make_pair
+#define vt vector
+#define ins insert
+#define __int128 int128
+#define all(x) begin(x), end(x)
+#define rall(x) rbegin(x), rend(x)
+#define sz(x) (int)x.size()
+#define REP(i, a) for (int i = 0; i < a; i++)
+#define pii pair<int, int>;
+#define setprf(x) setprecision(x) << fixed
+#define setpr(x) setprecision(x)
+#define pll pair<ll, ll>;
+
+using namespace std;
+
+typedef vector<ll> vl;
+typedef pair<ll,ll> pl;
+typedef long double ld;
+typedef complex<ld> cd;
+
+void solve()
+{
+    ll n,m,x,k; string s;
+    cin>>n>>m>>x>>k>>s;
+    ll e =0,o=0;
+    REP (i,k) {
+        if (s[i]=='E') {
+            e++;
+        }
+        else o++;
+    }
+    ll o2=o, e2=e;
+    o*=((m+1)/2);
+    e*=(m/2);
+    o=min(o,min(x*((m+1)/2),o2));
+    e=min(e,min(x*(m/2),e2));
+    if (o+e>=n) cout << "yes\n";
+    else cout << "no\n";
+}
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0), cout.tie(0);
+   	ll t;
+    	cin >> t;
+    	while (t--)
+    	{
+        	solve();
+    	}
+    	return 0;
+}
