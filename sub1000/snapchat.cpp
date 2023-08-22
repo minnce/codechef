7e41d511f3d8c236e70d864bce23e26877b5e04c
Chris Minn <105378107+minnce@users.noreply.github.com>
2023-08-22 10:03:39 -0500

    Create snapchat.cpp

---
+++ sub1000/snapchat.cpp
@@ -0,0 +1,63 @@
+#pragma GCC optimize("O2,no-stack-protector,unroll-loops,fast-math")
+#include <iostream>
+#include <bits/stdc++.h>
+#define ll long long
+#define db double
+#define MOD 1000000007
+#define PB push_back
+#define F first
+#define S second
+#define uset unordered_set
+#define umap unordered_map
+#define pq priority_queue
+#define MP make_pair
+#define vt vector
+#define ins insert
+#define __int128 int128
+#define all(x) begin(x), end(x)
+#define rall(x) rbegin(x), rend(x)
+#define sz(x) (int)x.size()
+#define REP(i, a) for (int i = 0; i < a; i++)
+#define pii pair<int, int>;
+#define setprf(x) setprecision(x) << fixed
+#define setpr(x) setprecision(x)
+#define pll pair<ll, ll>;
+
+using namespace std;
+
+typedef vector<ll> vl;
+typedef pair<ll,ll> pl;
+typedef long double ld;
+typedef complex<ld> cd;
+
+void solve()
+{
+    ll res = 0;
+    ll curr = 0;
+    ll n;
+    cin>>n;
+    ll arr[n], arr2[n];
+    REP (i,n) {
+        cin >> arr[i];
+    }
+    REP (i,n) cin >> arr2[i];
+    REP (i,n) {
+        if (arr[i]!=0&&arr2[i]!=0) curr++;
+        else curr=0;
+        res=max(res,curr);
+    }
+    cout << res << endl;
+}
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0), cout.tie(0);
+   	ll t;
+    	cin >> t;
+    	while (t--)
+    	{
+        	solve();
+    	}
+    	return 0;
+}
