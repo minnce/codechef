77cc7e5bed313ba760a8dc93cb6d5f542e158077
Chris Minn <105378107+minnce@users.noreply.github.com>
2023-08-22 10:05:28 -0500

    Create countOfMaximum.cpp

---
+++ 1000-1200/countOfMaximum.cpp
@@ -0,0 +1,67 @@
+#pragma GCC optimize("O2,no-stack-protector,unroll-loops,fast-math")
+#include <iostream>
+#include <bits/stdc++.h>
+#define ll long long
+#define db double
+#define MOD 1000000007
+#define PB push_back
+#define F first
+#define S second
+#define uset unordered_set
+#define umap unordered_map
+#define pq priority_queue
+#define MP make_pair
+#define vt vector
+#define ins insert
+#define __int128 int128
+#define all(x) begin(x), end(x)
+#define rall(x) rbegin(x), rend(x)
+#define sz(x) (int)x.size()
+#define REP(i, a) for (int i = 0; i < a; i++)
+#define pii pair<int, int>;
+#define setprf(x) setprecision(x) << fixed
+#define setpr(x) setprecision(x)
+#define pll pair<ll, ll>;
+
+using namespace std;
+
+typedef vector<ll> vl;
+typedef pair<ll,ll> pl;
+typedef long double ld;
+typedef complex<ld> cd;
+
+void solve()
+{
+    ll n;
+    cin>>n;
+    map<ll,ll>m;
+    REP (i,n) {
+        ll d;
+        cin>>d;
+        m[d]++;
+    }
+    ll res = 0,res2=0;
+    for (auto e:m) {
+        if (e.S==res&&e.F<res2) {
+            res2=e.F;
+        }
+        else if (e.S>res) {
+            res2=e.F;
+            res=e.S;
+        }
+    }
+    cout << res2 << " " << res << endl;
+}
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0), cout.tie(0);
+   	ll t;
+    	cin >> t;
+    	while (t--)
+    	{
+        	solve();
+    	}
+    	return 0;
+}
