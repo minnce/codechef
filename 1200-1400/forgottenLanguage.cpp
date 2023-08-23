8c291e5ebf37b396539124aa502945a67b548d1c
Chris Minn <105378107+minnce@users.noreply.github.com>
2023-08-23 10:36:04 -0500

    Create forgottenLanguage.cpp

---
+++ 1200-1400/forgottenLanguage.cpp
@@ -0,0 +1,71 @@
+#pragma GCC optimize("O2,no-stack-protector,unroll-loops,fast-math")
+#include <iostream>
+#include <bits/stdc++.h>
+#define ll long long
+#define db double
+#define MOD 1000000007
+#define PB push_back
+#define F first
+#define S second
+#define uset unordered_set
+#define umap unordered_map
+#define pq priority_queue
+#define MP make_pair
+#define vt vector
+#define ins insert
+#define __int128 int128
+#define all(x) begin(x), end(x)
+#define rall(x) rbegin(x), rend(x)
+#define sz(x) (int)x.size()
+#define REP(i, a) for (int i = 0; i < a; i++)
+#define pii pair<int, int>;
+#define setprf(x) setprecision(x) << fixed
+#define setpr(x) setprecision(x)
+#define pll pair<ll, ll>;
+
+using namespace std;
+
+typedef vector<ll> vl;
+typedef pair<ll,ll> pl;
+typedef long double ld;
+typedef complex<ld> cd;
+
+void solve()
+{
+    ll n,k;
+    cin>>n>>k;
+    map<string,int> m;
+    vector<string> v;
+    REP (i,n) {
+        string s;
+        cin>>s;
+        v.PB(s);
+    }
+    REP (i,k) {
+        ll d;
+        cin>>d;
+        REP (j,d) {
+            string s;
+            cin>>s;
+            m[s]++;
+        }
+    }
+    REP (i,n) {
+        if (m[v[i]]) cout << "YES ";
+        else cout << "NO ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0), cout.tie(0);
+   	ll t;
+    	cin >> t;
+    	while (t--)
+    	{
+        	solve();
+    	}
+    	return 0;
+}
