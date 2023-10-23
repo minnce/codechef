559848834eeef61a2d70f1a739610724e013aa8e
Chris Minn <105378107+minnce@users.noreply.github.com>
2023-10-23 17:17:46 -0500

    Create gcdPrefix.cpp

---
+++ 1600-1800/gcdPrefix.cpp
@@ -0,0 +1,72 @@
+#pragma GCC optimize("O2,no-stack-protector,unroll-loops,fast-math")
+#include <iostream>
+#include <bits/stdc++.h>
+#define ll long long
+#define db double
+#define MOD 1000000007
+#define PB push_back
+#define F first
+#define S second
+#define uset unordered_set
+#define umap unordered_map
+#define pq priority_queue
+#define MP make_pair
+#define vt vector
+#define ins insert
+#define __int128 int128
+#define all(x) begin(x), end(x)
+#define rall(x) rbegin(x), rend(x)
+#define sz(x) (int)x.size()
+#define REP(i, a) for (int i = 0; i < a; i++)
+#define pii pair<int, int>;
+#define setprf(x) setprecision(x) << fixed
+#define setpr(x) setprecision(x)
+#define pll pair<ll, ll>;
+
+using namespace std;
+
+typedef vector<ll> vl;
+typedef pair<ll,ll> pl;
+typedef long double ld;
+typedef complex<ld> cd;
+
+void solve()
+{
+    ll n;
+    cin>>n;
+    ll curr;
+    ll arr[n];
+    bool flag= false;
+    REP (i,n) {
+        cin>>arr[i];
+        if (i==0) curr = arr[i];
+        else {
+            if (gcd(curr,arr[i])!=arr[i]) {
+                flag = true;
+            }
+            else {
+                curr = gcd(curr,arr[i]);
+            }
+        }
+    }
+    if (flag) cout << -1 << endl;
+    else {
+        REP (i,n){
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0), cout.tie(0);
+   	ll t;
+    	cin >> t;
+    	while (t--)
+    	{
+        	solve();
+    	}
+    	return 0;
+}
